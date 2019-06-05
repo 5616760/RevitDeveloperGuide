e75d461394e518bf33a8c8fbba28e6a13d87dcfb
ZHANG.JJ <5616760@qq.com>
2019-06-05 15:02:20 +0800

    添加项目文件。

---
+++ _001HelloWorld/SolidTest.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _001HelloWorld {
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class SolidTest : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+
+            return Result.Succeeded;
+        }
+    }
+}
